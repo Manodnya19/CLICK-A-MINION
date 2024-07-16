5fc7c4186197ae747872a13c5361ad8f0ad833ce
Manodnya19 <manodnyagaikwad@gmail.com>
2024-07-15 20:02:09 -0700

    levels-added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from 'react';
 import Minion from './Minion';
 import '../App.css';
 
-export default function Game(){
-  
+export default function Game() {
   const [minions, setMinions] = useState(new Array(6).fill({ isVisible: false, type: 0 }));
   const [score, setScore] = useState(0);
-
+  const [time, setTime] = useState(1000); // Initial interval time
+  const [isFlipped, setIsFlipped] = useState(false);
+  let [count, setCount] = useState(1)
   useEffect(() => {
     const timer = setInterval(() => {
       const newMinions = minions.map(() => ({
         isVisible: Math.random() > 0.7,
-        type: Math.floor(Math.random() * 3) 
+        type: Math.floor(Math.random() * 3)
       }));
       setMinions(newMinions);
-    }, 800);
+    }, time);
 
     return () => clearInterval(timer);
-  }, [minions]);
+  }, [time, minions]);
 
   const handleMinionClick = (index) => {
     if (minions[index].isVisible) {
@@ -27,20 +28,38 @@ export default function Game(){
       setMinions(newMinions);
     }
   };
+
   const handleReset = () => {
     setScore(0);
     setMinions(new Array(6).fill({ isVisible: false, type: 0 }));
   };
-  const [isFlipped, setIsFlipped] = useState(false);
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
   };
 
+  const handleLevelUp = () => {
+    if (time > 100) {
+      setTime(time - 100);
+      setCount(count + 1)
+    }
+    // console.log(time);
+  };
+
+  const handleLevelDown = () => {
+    if (time >= 100 && time < 1000) {
+      setTime(time + 100);
+      setCount(count - 1)
+    }
+    // console.log(time);
+  }
+  
+  
+
+  // console.log(newLevel);
   return (
-    <body className="game">
-      
-        <div className='content'>
+    <div className="game">
+      <div className='content'>
         <h1 className='main-heading'>CLICK-A-MINION!</h1>
         <h3 className='score'>SCORE: {score}</h3>
         <button className="reset-button" onClick={handleReset}>Reset</button>
@@ -52,14 +71,17 @@ export default function Game(){
               type={minion.type}
               onClick={() => handleMinionClick(index)}
             />
-            
           ))}
-          
         </div>
+        <div>
+        <h4 className='level-up'>Level {count}</h4>
+          <div className='level-up-buttons'>
+          <button className='level-up-button' onClick={handleLevelUp}>↑</button>
+          <button className='level-down-button' onClick={handleLevelDown}>↓</button>
+          </div>
+        </div>
+        
       </div>
-    </body>
-    
+    </div>
   );
-};
-
-
+}
